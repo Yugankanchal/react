7ec7ffc41630c77e569d132ea7da159689a20cbe
Yugank <yugankanchal27@gmail.com>
2024-03-11 00:29:29 +0530

    basic project of changing background color by clicking on the buttons

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const username = 'yugank anchal'
+
+const anotherUser = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'welcome to google ', username
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  anotherUser
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
